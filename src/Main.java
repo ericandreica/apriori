8ca76a42fb9ed75186ee03a8fb71e16aeb9ef173
ionutmarisca <ionutmarisca@gmail.com>
2019-01-07 11:36:58 +0200

    Added Apriori Red. Tranz. on Threads with ExecutorService Updated Main to show elapsed time in ms.
    Signed-off-by: ionutmarisca <ionutmarisca@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,25 @@ public class Main {
 
 
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        System.out.println("###Apriori Basic###");
+        AprioriBasic aprioriBasic = new AprioriBasic("//Users//ionutmarisca//Documents//Projects//apriori//src//test2", 0.03,0.4);
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println("Elapsed time (ms): " + elapsedTime);
 
-        AprioriBasic aprioriBasic = new AprioriBasic("E:\\facultate\\a1s1\\datamining\\apriori\\src\\test2", 0.03,0.4);
+        startTime = System.currentTimeMillis();
+        System.out.println("###Apriori Red. Tranz.###");
+        AprioriRedTranz aprioriRedTranz = new AprioriRedTranz("//Users//ionutmarisca//Documents//Projects//apriori//src//test2", 0.03,0.4);
+        stopTime = System.currentTimeMillis();
+        elapsedTime = stopTime - startTime;
+        System.out.println("Elapsed time (ms): " + elapsedTime);
 
-        AprioriRedTranz aprioriRedTranz = new AprioriRedTranz("E:\\facultate\\a1s1\\datamining\\apriori\\src\\test2", 0.03,0.4);
-
-
+        startTime = System.currentTimeMillis();
+        System.out.println("###Apriori Red. Tranz. - Threads###");
+        AprioriRedTranzThreads aprioriRedTranzThreads = new AprioriRedTranzThreads("//Users//ionutmarisca//Documents//Projects//apriori//src//test2", 0.03,0.4);
+        stopTime = System.currentTimeMillis();
+        elapsedTime = stopTime - startTime;
+        System.out.println("Elapsed time (ms): " + elapsedTime);
     }
 }
