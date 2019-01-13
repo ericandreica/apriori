69035d2598c137d0aaf3fd38f8abbb5dd23353e9
Artyomska <dragosbad2009@gmail.com>
2019-01-13 19:23:08 +0200

    New branch with some modifications

--- src/AprioriBasicThreads.java
+++ src/AprioriBasicThreads.java
@@ -1,16 +1,23 @@
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
-class AprioriBasic {
+class AprioriBasicThreads {
+
 
     private List<Set<String>> transactions;
     private Map<Integer, Map<Set<String>, Integer>> kitemset;
     private double minsup;
     private double minconf;
     private double nrTr;
+    private int k;
 
     private BaseClass base;
 
-    AprioriBasic(String file, double minsup, double minconf) {
+    AprioriBasicThreads(String file, double minsup, double minconf) {
 
         boolean isReduced = false;
         this.base = new BaseClass(file, minsup, minconf, isReduced);
@@ -21,29 +28,35 @@ class AprioriBasic {
         this.nrTr = base.getNrTr();
 
 
-        aprioriBasicAlgorithm();
+        aprioriBasicAlgorithmThreads();
 
         //System.out.println("Numar total iteratii: "+nrit);
 
     }
 
-    private void aprioriBasicAlgorithm() {
-        int k = 1;
+    private void aprioriBasicAlgorithmThreads() {
+        k = 1;
         while (!kitemset.get(k).isEmpty()) {
-            int nrit=0;
+            AtomicInteger nrit = new AtomicInteger();
             //base.showKSet(k);
             Map<Set<String>, Integer> candMap = base.generateCand(k);
             if (candMap.isEmpty()) {
                 k++;
                 break;
             }
+
+
+            ExecutorService executorService = Executors.newFixedThreadPool(4);
+            List<Callable<Object>> todo = new ArrayList<Callable<Object>>(transactions.size());
             for (Set transaction : transactions) {
-                for (Set cset : candMap.keySet()) {
-                    nrit++;
-                    if (transaction.containsAll(cset))
-                        candMap.put(cset, candMap.get(cset) + 1);
-                }
+                todo.add(Executors.callable(() -> nrit.set(parseTransaction(nrit.get(), candMap, transaction))));
             }
+            try {
+                List<Future<Object>> answers = executorService.invokeAll(todo);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             System.out.println((k+1)+"---"+nrit);
             Map<Set<String>, Integer> kmap = new HashMap<>();
             for (Set cset : candMap.keySet()) {
@@ -59,4 +72,17 @@ class AprioriBasic {
         base.setKitemset(kitemset);
         base.calculateConfidence(k);
     }
+
+    private int parseTransaction(int nrit, Map<Set<String>, Integer> candMap, Set transaction) {
+
+        for (Set cset : candMap.keySet()) {
+            nrit++;
+            if (transaction.containsAll(cset))
+                candMap.put(cset, candMap.get(cset) + 1);
+        }
+
+        return nrit;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,39 @@
+import java.util.*;
+
 public class Main {
 
 
     public static void main(String[] args) {
+
+        String filepath = "C://Users//Artyomska//IdeaProjects//apriori//src//test2";
+
+        double minsup = 0.03;
+        double minconf = 0.4;
+
         long startTime = System.currentTimeMillis();
         System.out.println("###Apriori Basic###");
-        AprioriBasic aprioriBasic = new AprioriBasic("//Users//ionutmarisca//Documents//Projects//apriori//src//test2", 0.03,0.4);
+        AprioriBasic aprioriBasic = new AprioriBasic(filepath, minsup,minconf);
         long stopTime = System.currentTimeMillis();
         long elapsedTime = stopTime - startTime;
         System.out.println("Elapsed time (ms): " + elapsedTime);
 
         startTime = System.currentTimeMillis();
+        System.out.println("###Apriori Basic. - Threads###");
+        AprioriBasicThreads aprioriBasicThreads = new AprioriBasicThreads(filepath, minsup,minconf);
+        stopTime = System.currentTimeMillis();
+        elapsedTime = stopTime - startTime;
+        System.out.println("Elapsed time (ms): " + elapsedTime);
+
+        startTime = System.currentTimeMillis();
         System.out.println("###Apriori Red. Tranz.###");
-        AprioriRedTranz aprioriRedTranz = new AprioriRedTranz("//Users//ionutmarisca//Documents//Projects//apriori//src//test2", 0.03,0.4);
+        AprioriRedTranz aprioriRedTranz = new AprioriRedTranz(filepath, minsup,minconf);
         stopTime = System.currentTimeMillis();
         elapsedTime = stopTime - startTime;
         System.out.println("Elapsed time (ms): " + elapsedTime);
 
         startTime = System.currentTimeMillis();
         System.out.println("###Apriori Red. Tranz. - Threads###");
-        AprioriRedTranzThreads aprioriRedTranzThreads = new AprioriRedTranzThreads("//Users//ionutmarisca//Documents//Projects//apriori//src//test2", 0.03,0.4);
+        AprioriRedTranzThreads aprioriRedTranzThreads = new AprioriRedTranzThreads(filepath, minsup,minconf);
         stopTime = System.currentTimeMillis();
         elapsedTime = stopTime - startTime;
         System.out.println("Elapsed time (ms): " + elapsedTime);
